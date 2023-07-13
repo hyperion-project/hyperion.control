dd71c3a99e359bb1431e09ca9c6596b91c903dfc
Andrea Ghensi <andrea.ghensi@gmail.com>
2023-07-13 08:52:21 +0200

    refactor: more PEP8

--- service.py
+++ service.py
@@ -104,7 +104,7 @@ class Hyperion:
             do_initial_wizard()
             set_setting('firstRun', 'False')
 
-        if self.enableHyperion:
+        if self.enable_hyperion:
             self.connection.send_component_state("ALL", True)
 
         self.initialized = True
@@ -113,41 +113,26 @@ class Hyperion:
         # update settings, update the connection and updateState().
         self.ip = get_setting('ip')
         self.port = get_setting('port')
-        self.videoModeEnabled = get_bool_setting('videoModeEnabled')
-        self.enableHyperion = get_bool_setting('enableHyperion')
-        self.disableHyperion = get_bool_setting('disableHyperion_player_pause')
-        self.authToken = get_setting('authToken')
-        self.opt_targetComp = int_to_comp_string(int(get_setting('targetComponent')))
-        self.opt_screensaverEnabled = get_bool_setting('screensaverEnabled')
-        self.opt_videoEnabled = get_bool_setting('videoEnabled')
-        self.opt_audioEnabled = get_bool_setting('audioEnabled')
-        self.opt_pauseEnabled = get_bool_setting('pauseEnabled')
-        self.opt_menuEnabled = get_bool_setting('menuEnabled')
+        self.video_mode_enabled = get_bool_setting('videoModeEnabled')
+        self.enable_hyperion = get_bool_setting('enableHyperion')
+        self.disable_hyperion = get_bool_setting('disableHyperion_player_pause')
+        self.auth_token = get_setting('authToken')
+        self.opt_target_comp = int_to_comp_string(int(get_setting('targetComponent')))
+        self.opt_screensaver_enabled = get_bool_setting('screensaverEnabled')
+        self.opt_video_enabled = get_bool_setting('videoEnabled')
+        self.opt_audio_enabled = get_bool_setting('audioEnabled')
+        self.opt_pause_enabled = get_bool_setting('pauseEnabled')
+        self.opt_menu_enabled = get_bool_setting('menuEnabled')
         self.opt_debug = get_bool_setting('debug')
-        self.opt_showChangelogOnUpdate = get_bool_setting('showChangelogOnUpdate')
+        self.opt_show_changelog_on_update = get_bool_setting('showChangelogOnUpdate')
         self.tasks = get_setting('tasks')
 
         if self.opt_debug:
-            log('Settings updated!')
-            log('Hyperion ip:           %s' % (self.ip))
-            log('Hyperion port:         %s' % (self.port))
-            log('Enable H on start:     %s' % (self.enableHyperion))
-            log('Disable H on stop:     %s' % (self.disableHyperion))
-            log('VideoMode enabled:     %s' % (self.videoModeEnabled))
-            log('Hyperion target comp:  %s' % (self.opt_targetComp))
-            log('Screensaver enabled:   %s' % (self.opt_screensaverEnabled))
-            log('Video enabled:         %s' % (self.opt_videoEnabled))
-            log('Audio enabled:         %s' % (self.opt_audioEnabled))
-            log('Pause enabled:         %s' % (self.opt_pauseEnabled))
-            log('Menu enabled:          %s' % (self.opt_menuEnabled))
-            log('Debug enabled:         %s' % (self.opt_debug))
-            log('ChangelogOnUpdate:     %s' % (self.opt_showChangelogOnUpdate))
-            log('tasks:                 %s' % (self.tasks))
-
+            self._log_settings()
         self.connection.update_url(self.ip, self.port)
-        if validate_auth_token(self.authToken):
-            self.connection.update_header(self.authToken)
-        elif self.authToken != "":
+        if validate_auth_token(self.auth_token):
+            self.connection.update_header(self.auth_token)
+        elif self.auth_token != "":
             notify_user(get_localized_string(32105))
         # do just once on startup, we might want to show a changelog
         if not self.initialized:
@@ -160,12 +145,29 @@ class Hyperion:
             set_setting('tasks', '0')
             do_ssdp_discovery()
 
+    def _log_settings(self):
+        log('Settings updated!')
+        log(f'Hyperion ip:           {self.ip}')
+        log(f'Hyperion port:         {self.port}')
+        log(f'Enable H on start:     {self.enable_hyperion}')
+        log(f'Disable H on stop:     {self.disable_hyperion}')
+        log(f'VideoMode enabled:     {self.video_mode_enabled}')
+        log(f'Hyperion target comp:  {self.opt_target_comp}')
+        log(f'Screensaver enabled:   {self.opt_screensaver_enabled}')
+        log(f'Video enabled:         {self.opt_video_enabled}')
+        log(f'Audio enabled:         {self.opt_audio_enabled}')
+        log(f'Pause enabled:         {self.opt_pause_enabled}')
+        log(f'Menu enabled:          {self.opt_menu_enabled}')
+        log(f'Debug enabled:         {self.opt_debug}')
+        log(f'ChangelogOnUpdate:     {self.opt_show_changelog_on_update}')
+        log(f'tasks:                 {self.tasks}')
+
     def daemon(self):
         # Keep the Hyperion class alive, aborts if Kodi requests it
         while not self.monitor.abortRequested():
             if self.monitor.waitForAbort():
                 # last steps before script shutdown
-                if self.disableHyperion:
+                if self.disable_hyperion:
                     self.connection.send_component_state("ALL", False)
 
                 log("Hyperion-control stopped")
@@ -177,32 +179,32 @@ class Hyperion:
         if self.monitor.is_screensaver_active():
             if self.opt_debug:
                 notify_user("monitor.isScreensaverActive", 1000)
-            comp_state = bool(self.opt_screensaverEnabled)
+            comp_state = self.opt_screensaver_enabled
         elif self.player.paused:
             if self.opt_debug:
-                notify_user("player.isPausing", 1000)
-            comp_state = bool(self.opt_pauseEnabled)
+                notify_user("player.paused", 1000)
+            comp_state = self.opt_pause_enabled
         elif self.player.isPlayingAudio():
             if self.opt_debug:
                 notify_user("player.isPlayingAudio", 1000)
-            comp_state = bool(self.opt_audioEnabled)
+            comp_state = self.opt_audio_enabled
         elif self.player.isPlaying():
             if self.opt_debug:
                 notify_user("player.isPlayingVideo", 1000)
-            comp_state = bool(self.opt_videoEnabled)
+            comp_state = self.opt_video_enabled
         else:
             if self.opt_debug:
                 notify_user("MENU mode", 1000)
-            comp_state = bool(self.opt_menuEnabled)
+            comp_state = self.opt_menu_enabled
 
         # update comp state just if required
         if self.prev_comp_state != comp_state:
-            self.connection.send_component_state(self.opt_targetComp, comp_state)
+            self.connection.send_component_state(self.opt_target_comp, comp_state)
             self.prev_comp_state = comp_state
 
         # update stereoscopic mode always, better apis for detection available?
         # Bug: race condition, return of jsonapi has wrong gui state after onPlayBackStopped after a 3D movie
-        if self.videoModeEnabled:
+        if self.video_mode_enabled:
             new_mode = get_stereoscopic_mode()
             if self.prev_video_mode != new_mode:
                 self.connection.send_video_mode(new_mode)
